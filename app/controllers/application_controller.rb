2b2388aec5792aac1e01be865360c4dd40221216
Gianpietro Lavado <gianpietro1@gmail.com>
2015-06-01 17:33:49 -0500

    welcome look & feel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,12 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource_or_scope)
-    'welcome#index'
+  def after_sign_in_path_for(devise)
+    '/dashboard'
   end
 
-  def after_sign_out_path_for(resource_or_scope)
-    'devise/sessions#new'
+  def after_sign_out_path_for(devise)
+    '/'
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
     root :to => 'devise/sessions#new'
   end
 
-  
+  get '/dashboard' => 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
