621b735aeae059237b470e6a61d93f5c703bfb77
Gianpietro Lavado <gianpietro1@gmail.com>
2015-06-02 19:16:02 -0500

    local -> Claro

--- app/controllers/ppmroutes_controller.rb
+++ app/controllers/ppmroutes_controller.rb
@@ -2,9 +2,9 @@ class PpmroutesController < ApplicationController
 
   def index
     @table = Ppmroute.routescale_table
-    rescue => e
-      flash[:error] = "Error de conexión con Prime Performance Manager"
-      redirect_to '/ppmreports'
+    #rescue => e
+    #  flash[:error] = "Error de conexión con Prime Performance Manager"
+    #  redirect_to '/ppmreports'
   end
 
 end
--- app/models/ppmroute.rb
+++ app/models/ppmroute.rb
@@ -6,9 +6,10 @@ class Ppmroute
   require 'open-uri'
   require 'csv'
 
+
   def self.urlworks?
   # This method is not being used...
-    base_url = 'http://ppm16-demo.cisco.com:4440/ppm/rest/reports/IP+Protocols/Route+Summary/IP+Route+Summary?outputType=json'
+    # base_url = 'http://ppm16-demo.cisco.com:4440/ppm/rest/reports/IP+Protocols/Route+Summary/IP+Route+Summary?outputType=json'
     response = Net::HTTP.get_response(URI(base_url))
     if response.kind_of? Net::HTTPSuccess
       return 'YES'
@@ -18,21 +19,25 @@ class Ppmroute
   end
 
   def self.route_summary
-    base_url = 'http://ppm16-demo.cisco.com:4440/ppm/rest/reports/IP+Protocols/Route+Summary/IP+Route+Summary?outputType=json'
-    data = JSON.load(open(base_url))
-    return data
+    # base_url = 'http://ppm16-demo.cisco.com:4440/ppm/rest/reports/IP+Protocols/Route+Summary/IP+Route+Summary?outputType=json'
+    base_url = 'https://aquiroga:Cl4r0peru51@172.19.212.8:4440/ppm/rest/reports/IP+Protocols/Route+Summary/IP+Route+Summary?outputType=json&durationSelect=lastHour'
+    data = RestClient::Request.execute(:url => base_url , :method => :get, :verify_ssl => false)
+    data_parsed = JSON.load(data)
+    return data_parsed
   end
 
   def self.inventory
-    inventory_array = CSV.read(Rails.root + "public/inventory.csv")
+    # inventory_array = CSV.read(Rails.root + "public/inventory_local.csv")
+    inventory_array = CSV.read(Rails.root + "public/inventory-claro.csv")
   end
 
   def self.scalability
-    scalability_array = CSV.read(Rails.root + "public/scalability.csv")
+    # scalability_array = CSV.read(Rails.root + "public/scalability_local.csv")
+    scalability_array = CSV.read(Rails.root + "public/scalability-claro.csv")
   end  
 
   def self.routescale_table
-    table = Set.new []
+    table = Array.new []
     inventory.map do |device|
       
       scalability.map do |device_scale|
@@ -54,17 +59,17 @@ class Ppmroute
       end
 
       if (Float(@scale) != nil rescue false ) && (Float(@routes) != nil rescue false)
-        @status = (@routes / @scale ) * 100.00
+        @status = ((@routes / @scale ) * 100.00 ).round(2)
       else
         @status = 0.00
       end
 
       hash = Hash[node: device[0], model: device[1], scale: @scale, routes: @routes, status: @status]
 
-      table.add(hash) 
+      table << hash
     end
 
-    return table
+    return table.sort! { |a,b| b[:status] <=> a[:status] }
   end
 
 end
